a3971c197c94a41d0e2da0d362d2c8a7de9471f9
Suratan63011017 <folnw2545@gmail.com>
2022-11-17 11:45:38 +0700

    Create 63.py

---
+++ 63.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        numrows = len(obstacleGrid)
+        numcols = len(obstacleGrid[0])
+        
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
+        obstacleGrid[0][0] = 1
+        for i in range(1,numrows,1):
+            obstacleGrid[i][0] = int(obstacleGrid[i-1][0] == 1 and obstacleGrid[i][0] == 0)
+
+        for i in range(1,numcols,1):
+            obstacleGrid[0][i] = int(obstacleGrid[0][i-1] == 1 and obstacleGrid[0][i] == 0)
+
+        for i in range(1,numrows,1):
+            for j in range(1,numcols,1):
+                obstacleGrid[i][j] = (obstacleGrid[i][j-1] + obstacleGrid[i-1][j]) * int(obstacleGrid[i][j] == 0)  
+        return obstacleGrid[-1][-1]
