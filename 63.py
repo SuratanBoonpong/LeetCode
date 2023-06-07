0a4d5cbfdf92c9e269836dad808b5585b78a7b6e
Suratan Boonpong <69116231+Suratan63011017@users.noreply.github.com>
2023-06-07 11:19:03 +0700

    Update 63.py

--- 63.py
+++ 63.py
@@ -7,13 +7,13 @@ class Solution:
             return 0
         
         obstacleGrid[0][0] = 1
-        for i in range(1,numrows,1):
+        for i in range(1,numrows):
             obstacleGrid[i][0] = int(obstacleGrid[i-1][0] == 1 and obstacleGrid[i][0] == 0)
 
-        for i in range(1,numcols,1):
+        for i in range(1,numcols):
             obstacleGrid[0][i] = int(obstacleGrid[0][i-1] == 1 and obstacleGrid[0][i] == 0)
 
-        for i in range(1,numrows,1):
-            for j in range(1,numcols,1):
+        for i in range(1,numrows):
+            for j in range(1,numcols):
                 obstacleGrid[i][j] = (obstacleGrid[i][j-1] + obstacleGrid[i-1][j]) * int(obstacleGrid[i][j] == 0)  
         return obstacleGrid[-1][-1]
